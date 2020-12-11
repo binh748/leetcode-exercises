28bc7aea0408a068dcbd1733857a0655e4256790
binh748 <binh.hoang7@gmail.com>
2020-12-11 13:30:48 -0500

    Complete SQL question 601. Human Traffic of Stadium (LeetCode hard)

--- sql_solutions.sql
+++ sql_solutions.sql
@@ -4,6 +4,7 @@ more elegant version. */
 
 -- 1454. Active Users
 -- This is a difficult problem that took me some time to understand.
+-- This type of problem is called the gaps and islands problem.
 -- Here are links to resources that helped me figure out this problem:
 -- https://leetcode.com/problems/active-users/discuss/644070/100-Faster-than-Submissions-Window-Functions-Documentation-Linked
 -- https://mattboegner.com/improve-your-sql-skills-master-the-gaps-islands-problem/
@@ -54,3 +55,80 @@ of distinct l2.login_dates that equals 5 (5 because that represents
      ON l1.id = a.id
   GROUP BY l1.id, l1.login_date
  HAVING COUNT(DISTINCT l2.login_date) = 5;
+
+SUM(CASE WHEN "Status" LIKE '*cancelled*' THEN 1 END)
+
+-- 601. Human Traffic of Stadium
+
+-- This is another gaps and islands problem.
+-- The thing I learned here is that it's possible to coincidentally have the
+-- same grouping number for your two categories; that means I have to add another layer
+-- to differentiate my groupings, which I did using a letter: H to indicate
+-- high traffic grouping and L to indicate low traffic.
+
+WITH status_cte AS (
+    SELECT *,
+           CASE WHEN people >= 100 THEN 'high_traffic' ELSE 'low_traffic' END AS status
+      FROM Stadium
+     ORDER BY id
+),
+dense_rank_cte AS (
+    SELECT *, DENSE_RANK() OVER(PARTITION BY status ORDER BY id) AS rankings
+      FROM status_cte
+     ORDER BY id
+),
+grouping_cte AS (
+    SELECT *, id - rankings AS groupings
+      FROM dense_rank_cte
+     ORDER BY id
+),
+grouping_differentiated_cte AS (
+    SELECT *, CASE WHEN status = 'high_traffic' THEN CONCAT(CAST(groupings AS char), 'H')
+                   ELSE CONCAT(CAST(groupings AS char), 'L') END AS groupings_diff
+      FROM grouping_cte
+     ORDER BY id
+),
+agg_cte AS (
+    SELECT groupings_diff, SUM(CASE WHEN people >= 100 THEN 1 ELSE 0 END) AS num_days_high_traffic
+      FROM grouping_differentiated_cte
+     GROUP BY 1
+    HAVING SUM(CASE WHEN people >= 100 THEN 1 ELSE 0 END) >= 3
+)
+SELECT id, visit_date, people
+  FROM grouping_differentiated_cte
+ WHERE groupings_diff IN (SELECT groupings_diff FROM agg_cte)
+ ORDER BY visit_date;
+
+-- Alternatively, I can make sure to only count the records that are the high_traffic
+-- records when I do my aggregation and only show the records that are high_traffic
+-- in my answer. This doesn't get rid of the problem of the groupings potentially
+-- mixing low_traffic and high_traffic records together; this just allows me to filter
+-- out the low_traffic records from groupings that have both types of records.
+
+ WITH status_cte AS (
+    SELECT *,
+           CASE WHEN people >= 100 THEN 'high_traffic' ELSE 'low_traffic' END AS status
+      FROM Stadium
+     ORDER BY id
+),
+dense_rank_cte AS (
+    SELECT *, DENSE_RANK() OVER(PARTITION BY status ORDER BY id) AS rankings
+      FROM status_cte
+     ORDER BY id
+),
+grouping_cte AS (
+    SELECT *, id - rankings AS groupings
+      FROM dense_rank_cte
+     ORDER BY id
+),
+agg_cte AS (
+    SELECT groupings, SUM(CASE WHEN people >= 100 THEN 1 ELSE 0 END) AS num_days_high_traffic
+      FROM grouping_cte
+     WHERE status = 'high_traffic'
+     GROUP BY 1
+    HAVING SUM(CASE WHEN people >= 100 THEN 1 ELSE 0 END) >= 3
+)
+SELECT id, visit_date, people
+  FROM grouping_cte
+ WHERE groupings IN (SELECT groupings FROM agg_cte) AND status = 'high_traffic'
+ ORDER BY visit_date;
